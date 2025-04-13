2cc9745ab627a2239b1eb710b2fb27a487f4449e
Nyxiie-Nek <ribeiro.cruz.victor@escola.pr.gov.br>
2025-04-13 01:26:49 -0300

    pequeno detalhe

---
+++ script/soundenabler.js
@@ -0,0 +1,8 @@
+<script>
+  document.addEventListener("click", () => {
+    const audio = document.getElementById("bgAudio");
+    audio.play().catch((e) => {
+      console.log("Autoplay bloqueado:", e);
+    });
+  }, { once: true }); // só ativa uma vez
+</script>
